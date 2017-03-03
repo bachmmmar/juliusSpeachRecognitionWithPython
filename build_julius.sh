35ae7fce5ecd5b9ec5158f7dc89449850cf03c7f
Martin Bachmann <bachmmar@gmail.com>
2017-03-03 20:14:13 +0100

    +: building dir

---
+++ build_julius.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+
+
+function checkSuccess() {
+    if [ $1 -ne 0 ]; then
+	echo "ERROR: ${2}"
+	exit 1
+    fi
+}
+
+function isCurrentDirBaseOfRepository() {
+    BUILD_DIR=$(pwd)/julius_build
+    INSTALL_DIR=$(pwd)/julius_install
+    SOURCE_DIR=$(pwd)/julius_source    
+    if [ ! -e ${BUILD_DIR} ]; then
+	echo "build directory does not exists! You have to execute the script from the repository base directory!"
+	exit 1
+    fi
+
+    
+}
+
+
+# Start Compile and installation
+isCurrentDirBaseOfRepository
+
+pushd $BUILD_DIR > /dev/null
+$SOURCE_DIR/configure --srcdir=${SOURCE_DIR} --prefix=${INSTALL_DIR} --enable-julius --enable-gramtools
+checkSuccess $? "Configure Julius"
+
+#make
+checkSuccess $? "Build Julius"
+
+#make install
+checkSuccess $? "Installing Julius to ${INSTALL_DIR}"
+
+
+
