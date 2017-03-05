b10300550f1e5d1ab5566e04f6a383490fd320c3
Martin Bachmann <bachmmar@gmail.com>
2017-03-05 19:10:54 +0100

    !: seems to be working

--- get_sentense.py
+++ get_sentense.py
@@ -3,17 +3,34 @@
 import sys
 import traceback
 import time
+import logging
 
 from julius import Julius
 
 
 def main():
+    l=logging.getLogger()
+    l.setLevel(logging.DEBUG)
+
+    # create console handler and set level to debug
+    ch = logging.StreamHandler()
+    ch.setLevel(logging.WARNING)
+
+    # create formatter
+    formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+
+    # add formatter to ch
+    ch.setFormatter(formatter)
+
+    # add ch to logger
+    l.addHandler(ch)
+
     x=Julius()
 
     try:
         while 1:
             x.getSentense()
-            time.sleep(0.1)            
+            time.sleep(0.1)
     except KeyboardInterrupt:
         print('Exiting...')
     except Exception:
--- julius.py
+++ julius.py
@@ -9,35 +9,41 @@ import pyjulius
 class Julius:
 
     def __init__(self):
+        self.is_connected = False
         # Initialize and try to connect
         self.jul_client_ = pyjulius.Client('localhost', 10500)
         try:
             self.jul_client_.connect()
         except pyjulius.ConnectionError:
-                print('Start julius as module first!')
-                sys.exit(1)
+            print('Start julius as module first!')
+            sys.exit(1)
 
-        # Start client wich starts word detection
+        # Start client which starts word detection
         self.jul_client_.start()
+        self.is_connected = True
+        print("Julius opened!")
 
     def __del__(self):
-        # unload class
-        print('Close connection to Julius!')
-        self.jul_client.stop()  # send the stop signal
-        self.jul_client.join()  # wait for the thread to die
-        self.jul_client.disconnect()  # disconnect from julius        
-        
-        
-    def extractCommands(result):
-        print('Sentence "%s" recognized with score %.2f', result, result.score)
+        if self.is_connected:
+            print('Close connection to Julius!')
+            self.jul_client_.stop()  # send the stop signal
+            self.jul_client_.join()  # wait for the thread to die
+            self.jul_client_.disconnect()  # disconnect from julius
 
-    def getSentense():
+
+    def extractCommands(self, result):
+        print('Sentence "{}" recognized with score {}'.format(result,result.score))
+
+
+    def getSentense(self):
         try:
-            result = client.results.get(False)
+            #ret = self.jul_client_.get_ret().get(False)
+            ret = self.jul_client_.results.get(False)
         except queue.Empty:
-            return ""
-        if isinstance(result, pyjulius.Sentence):
-            extractCommands(result)
-            return ""
+            return "empty"
+
+        if isinstance(ret, pyjulius.Sentence):
+            self.extractCommands(ret)
+            return "word"
         
 
